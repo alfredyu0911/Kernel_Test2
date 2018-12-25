15571e2b53f0750104cf0a61c702e6aac8b61db2
Alfred <alfredyu0911@gmail.com>
2018-12-25 17:38:19 +0800

    no message

--- io_bound.c
+++ io_bound.c
@@ -37,17 +37,17 @@ main()
         exit(0);
     }
 
-    /*===========================================================================*/
-    /*     prototype of the new system call is as follows                        */
-    /*     void get_process_switch_info(struct process_switch_info *)            */
-    /*===========================================================================*/
-    get_process_switch_info(&ps_info);    //new system call
+    // /*===========================================================================*/
+    // /*     prototype of the new system call is as follows                        */
+    // /*     void get_process_switch_info(struct process_switch_info *)            */
+    // /*===========================================================================*/
+    // get_process_switch_info(&ps_info);    //new system call
 
-    gettimeofday(&end, NULL);             //total time of existence - end
-    printf("The process spent %ld uses in the system after it stated its execution.\n", 
-    ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+    // gettimeofday(&end, NULL);             //total time of existence - end
+    // printf("The process spent %ld uses in the system after it stated its execution.\n", 
+    // ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
 
-    temp_ul=(ps_info.time.tv_sec * 1000000 + ps_info.time.tv_usec);
-    printf("The process has made %ld process switches\n", ps_info.counter);
-    printf("This process has idle %ul usecs\n", temp_ul); 
+    // temp_ul=(ps_info.time.tv_sec * 1000000 + ps_info.time.tv_usec);
+    // printf("The process has made %ld process switches\n", ps_info.counter);
+    // printf("This process has idle %ul usecs\n", temp_ul); 
 }
