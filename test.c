f9ca2938853bbe3c6c958fbcac9789e0f9b5d5fc
Alfred <alfredyu0911@gmail.com>
2018-12-28 17:57:22 +0800

    no message

--- test.c
+++ test.c
@@ -58,14 +58,13 @@ void main(int argc, char *argv[])
     /*===========================================================================*/
 
     unsigned int cs_counter;
-    time_t idle, exist;
+    struct timeval idle, exist;
 
     syscall(SYSTEM_CALL_ID, &cs_counter, &idle, &exist);
 
-    printf("The process spent %ld uses in the system after it stated its execution.\n", exist);
-    // ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+    printf("The process spent %ld uses in the system after it stated its execution.\n", (exist.tv_sec * 1000000 + exist.tv_usec));
 
     // temp_ul=(ps_info.time.tv_sec * 1000000 + ps_info.time.tv_usec);
     printf("The process has made %ld process switches\n", cs_counter);
-    printf("This process has idle %lu usecs\n", idle); 
+    printf("This process has idle %lu usecs\n", idle.tv_sec * 1000000 + idle.tv_usec); 
 }
