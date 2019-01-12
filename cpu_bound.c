1547ce90bae5c34d855d8da14059fef6d9c1f4f5
Alfred <alfredyu0911@gmail.com>
2019-01-12 19:21:50 +0800

    no message

--- cpu_bound.c
+++ cpu_bound.c
@@ -4,38 +4,24 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define TOTAL_ITERATION_NUM  100000000
-
-struct process_switch_info
-{
-    unsigned int   counter;
-    struct timeval time;
-};
+#define SYSTEM_CALL_ID 359
+#define TOTAL_CALCUATE_NUM 100000000
 
 main()
 {
-    int              a;
-    unsigned int     i=0;
-    struct timeval   start, end;
+    int a;
+    unsigned int i=0;
 
-    gettimeofday(&start, NULL);           //total time of existence - begin
-
-    for(i=1;i<=TOTAL_ITERATION_NUM;i++)
+    for ( i=1 ; i<= TOTAL_CALCUATE_NUM ; i++ )
         ++a;
+        
+    unsigned int cs_counter;
+    struct timeval idle, exist;
 
-    // /*===========================================================================*/
-    // /*     prototype of the new system call is as follows                        */
-    // /*     void get_process_switch_info(struct process_switch_info *)            */
-    // /*===========================================================================*/
-    // get_process_switch_info(&ps_info);    //new system call
+    syscall(SYSTEM_CALL_ID, &cs_counter, &idle, &exist);
 
-    // gettimeofday(&end, NULL);             //total time of existence - end 
-    // printf("The process spent %ld uses in the system after it stated its execution.\n", 
-    // ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+    printf("The process spent %ld uses in the system after it stated its execution.\n", (exist.tv_sec * 1000000 + exist.tv_usec));
 
-    // temp_ul=(ps_info.time.tv_sec * 1000000 + ps_info.time.tv_usec);
-    // printf("The process has made %ld process switches\n", ps_info.counter);
-    // printf("This process has idle %ul usecs\n", temp_ul); 
-
-
+    printf("The process has made %ld process switches\n", cs_counter);
+    printf("This process has idle %lu usecs\n", idle.tv_sec * 1000000 + idle.tv_usec); 
 }
--- io_bound.c
+++ io_bound.c
@@ -4,29 +4,22 @@
 #include <time.h>
 #include <sys/time.h>
 
-struct process_switch_info
-{
-    unsigned int   counter;
-    struct timeval time;
-};
-
+#define SYSTEM_CALL_ID 359
+#define TOTAL_IO_NUM 100000
 
 main()
 {
-    FILE             *out;
-    char             c;
-    int              a;
-    struct timeval   start, end;
+    FILE *out;
+    char c;
 
-    gettimeofday(&start, NULL);                 //total time of existence - begin
-
-    if((out=fopen("io_bound.data","w"))!=NULL)
+    if ( ( out=fopen("io_bound.data","w")) != NULL )
     {
-        c=38;
-        for(a=0; a<100000;a++)
+        c = 38;
+        int a;
+        for ( a=0 ; a < TOTAL_IO_NUM ; a++ )
         {
             putc(c,out);
-            if(++c>126)
+            if ( ++c > 126 )
             c=38;
         }
         fclose(out);
@@ -37,17 +30,13 @@ main()
         exit(0);
     }
 
-    // /*===========================================================================*/
-    // /*     prototype of the new system call is as follows                        */
-    // /*     void get_process_switch_info(struct process_switch_info *)            */
-    // /*===========================================================================*/
-    // get_process_switch_info(&ps_info);    //new system call
+    unsigned int cs_counter;
+    struct timeval idle, exist;
 
-    // gettimeofday(&end, NULL);             //total time of existence - end
-    // printf("The process spent %ld uses in the system after it stated its execution.\n", 
-    // ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)));
+    syscall(SYSTEM_CALL_ID, &cs_counter, &idle, &exist);
 
-    // temp_ul=(ps_info.time.tv_sec * 1000000 + ps_info.time.tv_usec);
-    // printf("The process has made %ld process switches\n", ps_info.counter);
-    // printf("This process has idle %ul usecs\n", temp_ul); 
+    printf("The process spent %ld uses in the system after it stated its execution.\n", (exist.tv_sec * 1000000 + exist.tv_usec));
+
+    printf("The process has made %ld process switches\n", cs_counter);
+    printf("This process has idle %lu usecs\n", idle.tv_sec * 1000000 + idle.tv_usec); 
 }
